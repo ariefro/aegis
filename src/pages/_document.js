3f3b7f20e1f7e5f32934e850dddf6b36784f3d81
rizqyhbb <rizqyhbb@gmail.com>
2022-11-02 13:59:42 +0700

    Adding icon, manifest and document for rendering pwa

---
+++ src/pages/_document.js
@@ -0,0 +1,32 @@
+/* eslint-disable react/function-component-definition */
+/* eslint-disable react/jsx-filename-extension */
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="manifest" href="manifest.json" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="application-name" content="Lakoste" />
+        <meta name="apple-mobile-web-app-title" content="Lakoste" />
+        <meta name="theme-color" content="#5B259F" />
+        <meta name="msapplication-navbutton-color" content="#5B259F" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
+        <meta name="msapplication-starturl" content="/" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
