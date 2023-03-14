bfe6134d1081a85ea1c4983089201b6be41bca53
rizqyhbb <rizqyhbb@gmail.com>
2023-03-14 07:37:17 +0700

    add notification API

--- src/pages/notification/index.jsx
+++ src/pages/notification/index.jsx
@@ -1,27 +1,49 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import moment from 'moment';
+import { useDispatch, useSelector } from 'react-redux';
 import { Layout } from '../../components';
-import { notifications } from '../../../dummy.json';
 import Card from '../../components/Pages/notification/Card';
+import { getNotifications } from '../../store/logSlice';
 
-const Notification = () => (
-  <div>
-    <h1 className="heading-l text-center text-dark-purple-1 mt-6 mb-14">
-      Notification
-    </h1>
-    {notifications.map((notif) => (
-      <Card
-        key={notif.id}
-        date={moment(notif.created_at).format('DD MMMM YYYY, hh.mm A')}
-        message={notif.message}
-        name={notif.name}
-        type={notif.type}
-      />
-    ))}
-  </div>
-);
+const Notification = () => {
+  const dispatch = useDispatch();
+  const { loading, notifications } = useSelector((state) => state.log);
+
+  useEffect(() => {
+    const init = async () => {
+      await dispatch(getNotifications()).unwrap();
+    };
+    init();
+  }, []);
+
+  return (
+    <div>
+      <h1 className="heading-l text-center text-dark-purple-1 mt-6 mb-14">
+        Notification
+      </h1>
+      {!notifications && loading && (
+        <>
+          <Card loading={loading} />
+          <Card loading={loading} />
+          <Card loading={loading} />
+        </>
+      )}
+      {notifications &&
+        notifications.map((notif) => (
+          <Card
+            key={notif.created_at}
+            date={moment(notif.created_at).format('DD MMMM YYYY, hh.mm A')}
+            message={notif.message}
+            name={notif.name}
+            type={notif.type}
+          />
+        ))}
+    </div>
+  );
+};
 
 Notification.getLayout = function getLayout(page) {
   return <Layout noNavigation>{page}</Layout>;
 };
+
 export default Notification;
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import logReducer from './logSlice';
 import transactionReducer from './transactionSlice';
 import userReducer from './userSlice';
 import walletReducer from './walletSlice';
 
 const store = configureStore({
   reducer: {
+    log: logReducer,
     transaction: transactionReducer,
     user: userReducer,
     wallet: walletReducer
