f4302bc5787710b361bba0423b1f445276899aea
ariefro <ariefromadhon26@gmail.com>
2023-07-17 07:40:48 +0700

    add title page

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -26,6 +26,7 @@ const MyApp = ({ Component, pageProps }) => {
   return (
     <Provider store={store}>
       <Head>
+        <title>Lakoste</title>
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
